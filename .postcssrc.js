ea797b65ab443261cff0b94383c52d0e2dcd95a8
leseraphina <leseraphina@gmail.com>
2022-03-22 12:26:34 +0900

    parcel end

---
+++ .postcssrc.js
@@ -0,0 +1,17 @@
+// ESM  -> import
+// nlde js ㄴㅡㄴ commonJs르ㄹ 사사용용한한다다. -> required()
+
+// import
+const autoprefixer = require('autoprefixer')
+// export
+// export{
+//   plugins:[
+//     autoprefixer
+//   ]
+// }
+
+module.exports ={
+  plugins:[
+    autoprefixer
+  ]
+  }
